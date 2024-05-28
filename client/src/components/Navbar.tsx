048c5c3aeaeeb6276b23d689064ced391b101a72
Frida Alstergren <frida@alstergren.com>
2024-05-28 19:53:43 +0200

    CSS variables, temporary logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,20 @@
 import "../styles/navbar.css"
+import logo from "../assets/SCOPE__5_-removebg-preview.png"
 
 export const Navbar = () => {
   return (
     <nav>
         <div className="nav-container">
-            <a href="" className="logo">
-                SCOPE
+            <a href="/" className="logo">
+                <img src={logo} alt="" />
             </a>
             <div className="nav-items">
                 <a href="">Pricing</a>
                 <a href="">Articles</a>
                 <a href="">About</a>
-                <button type="button" className="btn btn-light">Login</button>
+                <button type="button" className="btn btn-light" id="nav-login">
+                  <a href="/login">Login</a>
+                </button>
             </div>
         </div>
     </nav>
