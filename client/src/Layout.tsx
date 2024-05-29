ec20a1d9974ff3308aa13405a8b3eab6bf1b567f
Frida Alstergren <frida@alstergren.com>
2024-05-29 11:35:21 +0200

    Simple footer

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
 export const Layout = () => {
   return (
     <main>
       <Navbar />
-
       <Outlet />
+      <Footer />
     </main>
   );
 };
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import "../styles/footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="quick-links">
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms of Service</a>
+        <a href="#">Contact Us</a>
+        <a href="#">FAQs</a>
+      </div>
+      <div className="socials">
+        <a href="#">Facebook</a>
+        <a href="#">X</a>
+        <a href="#">Instagram</a>
+        <a href="#">LinkedIn</a>
+      </div>
+    </div>
+  );
+};
