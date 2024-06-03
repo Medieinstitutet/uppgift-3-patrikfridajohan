8693edda7b38741682f4f49bfe34ccb2bd83f02b
Johan Norr <johan.norr84@gmail.com>
2024-06-03 10:33:35 +0200

    new connection mysql

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import pool from './mysql';
+
 // import apiRouter from './routes/api';
 
 dotenv.config();
@@ -11,6 +13,21 @@ app.use(express.json()); // Middleware to parse JSON bodies
 
 // Define your API routes
 // app.use('/api', apiRouter);
+
+async function testMySQLConnection() {
+    try {
+        const [rows, fields]: [any[], any] = await pool.query('SELECT * FROM data_users LIMIT 1');
+
+        console.log('MySQL connection works!');
+        console.log('Retrieved data:', rows);
+    } catch (error) {
+        console.error('Error testing MySQL connection:', error);
+    }
+}
+
+testMySQLConnection();
+
+
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => { // Error handling middleware
     console.error(err.stack);
--- backend/mysql.ts
+++ backend/mysql.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import mysql from 'mysql2/promise'; 
+import mysql from 'mysql2/promise';
 
 dotenv.config();
 
@@ -8,57 +8,12 @@ const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
-const SQLlink = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 10,
     host: dbHost,
     user: dbUser,
     password: dbPass,
     database: dbName
 });
 
-SQLlink.connect()
-    .then(() => {
-        console.log('Connected to MySQL database');
-    })
-    .catch((err) => {
-        console.error('Connection failed: ', err);
-    });
-
-SQLlink.on('error', (err) => {
-    console.error('MySQL error occurred:', err.message);
-});
-
-export default SQLlink;
-
-
-// Alternativt men får felmeddelande när jag försöker installera mysql2/promise
-
-// const dotenv = require('dotenv');
-// const mysql = require('mysql2');
-
-// dotenv.config();
-
-// const dbHost = process.env.DB_HOST;
-// const dbName = process.env.DB_NAME;
-// const dbUser = process.env.DB_USER;
-// const dbPass = process.env.DB_PASS;
-
-// const SQLlink = mysql.createConnection({
-//     host: dbHost,
-//     user: dbUser,
-//     password: dbPass,
-//     database: dbName
-// });
-
-// SQLlink.connect((err) => {
-//     if (err) {
-//         console.error('Connection failed: ', err);
-//         return;
-//     }
-//     console.log('Connected to MySQL database');
-// });
-
-// SQLlink.on('error', (err) => {
-//     console.error('MySQL error occurred:', err.message);
-// });
-
-// module.exports = SQLlink;
+export default pool;
