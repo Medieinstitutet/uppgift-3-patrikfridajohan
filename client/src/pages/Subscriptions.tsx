cc556c403a6353edd169ac0bfc2af8029e5200cf
Johan Norr <johan.norr84@gmail.com>
2024-06-10 10:22:54 +0200

    Typesrcipt fixes

--- client/src/pages/Subscriptions.tsx
+++ client/src/pages/Subscriptions.tsx
@@ -1,22 +1,32 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getUseridfromcookie, getAllsubscriptions, getActiveSubscription, isLoggedIn } from '../services/authService';
 import "../styles/subscriptions.css";
 
-export const Subscriptions = () => {
-  const [plans, setPlans] = useState([]);
-  const [activeSubscriptionId, setActiveSubscriptionId] = useState(null);
-  const { loggedIn } = isLoggedIn();
+interface Plan {
+  id: string;
+  name: string;
+  price: number;
+  info: string;
+}
+
+export const Subscriptions: React.FC = () => {
+  const [plans, setPlans] = useState<Plan[]>([]);
+  const [activeSubscriptionId, setActiveSubscriptionId] = useState<string | null>(null);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
+    const { loggedIn } = isLoggedIn();
+    setLoggedIn(loggedIn);
+
     const userId = getUseridfromcookie();
-    console.log("UserID from cookie2:",userId);
+    console.log("UserID from cookie in plan:", userId);
 
     if (!loggedIn) {
-      window.location.href = '/login'; 
+      window.location.href = '/login';
       return;
     }
     if (!userId) {
-      window.location.href = '/login'; 
+      window.location.href = '/login';
       return;
     }
 
@@ -33,20 +43,20 @@ export const Subscriptions = () => {
 
     // Get active subscriptionID
     const fetchActiveSubscriptionId = async () => {
-        if (userId) {
-            try {
-                const subscription = await getActiveSubscription(userId);
-                setActiveSubscriptionId(subscription);
-            } catch (error) {
-                console.error('Error fetching active subscription:', error);
-            }
+      if (userId) {
+        try {
+          const subscription = await getActiveSubscription(userId);
+          setActiveSubscriptionId(subscription);
+        } catch (error) {
+          console.error('Error fetching active subscription:', error);
         }
+      }
     };
 
     fetchActiveSubscriptionId();
-  }, []);
+  }, [loggedIn]);
 
-  const handleSubscribe = (planId) => {
+  const handleSubscribe = (planId: string) => {
     console.log('Subscription button clicked for plan ID:', planId);
     // Write code to do magic when selecting plan here
   };
