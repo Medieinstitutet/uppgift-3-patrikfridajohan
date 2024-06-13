e92768ef5a7fc185f664020fe5095250e39f18a6
Frida Alstergren <frida@alstergren.com>
2024-06-13 11:33:36 +0200

    Added toast to sub cancelation

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -7,11 +7,10 @@ import {
   getSubscriptionData,
   cancelSubscription,
 } from "../services/authService";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Account: React.FC = () => {
-  // const [selection, setSelection] = useState<"dashboard" | "subscription" | "settings" | "support">("dashboard");
-  // const [subscriptionData, setSubscriptionData] = useState<{ name: string; price: number; active: boolean; enddate: Date; } | null>(null);
-
   const [firstname, setFirstname] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [endDate, setEndDate] = useState<string>("");
@@ -24,12 +23,10 @@ export const Account: React.FC = () => {
         const userId = getUseridfromcookie() ?? "";
 
         const userData = await getAllUserData(userId);
-        console.log("userData", userData);
         setFirstname(userData.firstname);
         setEmail(userData.email);
 
         const subData = await getActiveSubscriptionData(userId);
-        console.log("subData", subData);
         setEndDate(subData.enddate.split("T")[0]);
 
         if (subData.subscriptionid === 3) {
@@ -41,7 +38,6 @@ export const Account: React.FC = () => {
         }
 
         const subDataExp = await getSubscriptionData(subData.subscriptionid);
-        console.log("subDataExp", subDataExp);
         setPrice(subDataExp.price);
       } catch (error) {
         console.error("Error fetching subscription data:", error);
@@ -54,7 +50,17 @@ export const Account: React.FC = () => {
   const cancelSub = async () => {
     try {
       await cancelSubscription();
-      window. location. reload();
+      toast.info("Subscription cancelled successfully! You will have access to your current plan until the next payment is due.", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
     } catch {
       console.log("error canceling sub");
     }
@@ -62,6 +68,18 @@ export const Account: React.FC = () => {
 
   return (
     <section className="account-dashboard-section">
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <h2>My Profile</h2>
       <div className="account-info-section">
         <div className="account-info-container">
@@ -70,13 +88,24 @@ export const Account: React.FC = () => {
             <p>{email}</p>
           </header>
           <div className="subscription-details">
-            <p><strong>Current Plan:</strong> {tier}</p>
-            <p><strong>Price:</strong> ${price}/week</p>
-            <p><strong>Subscription Ends:</strong> {endDate}</p>
+            <p>
+              <strong>Current Plan:</strong> {tier}
+            </p>
+            <p>
+              <strong>Price:</strong> ${price}/week
+            </p>
+            <p>
+              <strong>Subscription Ends:</strong> {endDate}
+            </p>
           </div>
         </div>
         <div className="action-sec">
-          <button type="button" className="btn" onClick={cancelSub}>
+          <button
+            type="button"
+            className="btn"
+            id="cancel-btn"
+            onClick={cancelSub}
+          >
             Cancel Subscription
           </button>
         </div>
