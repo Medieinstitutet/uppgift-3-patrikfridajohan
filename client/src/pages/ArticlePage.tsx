e95256d07f258f92cc925ffb9f0e051de1c0c1df
Johan Norr <johan.norr84@gmail.com>
2024-06-13 15:10:07 +0200

    added unpaid message

--- client/src/pages/ArticlePage.tsx
+++ client/src/pages/ArticlePage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getArticleData, isLoggedIn } from "../services/authService";
+import { useParams, useNavigate } from "react-router-dom";
+import { getArticleData, isLoggedIn, getActiveSubscriptionData } from "../services/authService";
 import "../styles/article.css";
 
 interface Params {
@@ -12,67 +12,110 @@ interface ArticleData {
   title: string;
   shortinfo: string;
   longinfo: string;
+  added: string;
 }
 
 export const Article: React.FC = () => {
   const { id } = useParams<Params>();
+  const navigate = useNavigate();
   const [articleData, setArticleData] = useState<ArticleData | null>(null);
   const [allowed, setAllowed] = useState<boolean>(false);
+  const [subscriptionPaid, setSubscriptionPaid] = useState<boolean>(false);
+  const [stripeInvoiceUrl, setStripeInvoiceUrl] = useState<string>('');
   const { loggedIn } = isLoggedIn();
 
+  const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString();
+  };
+
   useEffect(() => {
-    const fetchArticle = async () => {
+    const fetchData = async () => {
       try {
-        if (id) {
-          const { articleData, allowed } = await getArticleData(id);
-          setArticleData(articleData);
-          setAllowed(allowed);
-        }
+        if (!loggedIn || !id) return;
+
+        // Fetch article data and allowed status
+        const { articleData, allowed } = await getArticleData(id);
+        setArticleData(articleData);
+        setAllowed(allowed);
+
+        // Fetch subscription payment status
+        const activeSubscriptionData = await getActiveSubscriptionData();
+        setSubscriptionPaid(activeSubscriptionData.payed === 1);
+        setStripeInvoiceUrl(activeSubscriptionData.stripeInvoiceUrl);
       } catch (error) {
-        console.error("Error fetching article data:", error);
+        console.error("Error fetching data:", error);
       }
     };
 
-    fetchArticle();
-  }, [id]);
+    fetchData();
+  }, [id, loggedIn]);
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  if (!loggedIn) {
+    return (
+      <div className="article-container">
+        <div className="not-allowed-message">
+          <h2>Authentication Required</h2>
+          <p>You need to login to view this article.</p>
+          <a className="btn" href="/login">
+            Login
+          </a>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="article-container">
-      {loggedIn ? (
-        allowed ? (
-          articleData ? (
-            <div>
+      {allowed ? (
+        <div>
+          {!subscriptionPaid && articleData && (
+            // Display only article title if subscription is unpaid
+            <>
               <h2>{articleData.title}</h2>
+              <div className="not-allowed-message">
+                <p>Payment unsuccessful. Your subscription is not paid. Please pay invoice to continue.</p>
+                <a className="btn" href={stripeInvoiceUrl} target="_blank" rel="noopener noreferrer">
+                  Pay Invoice
+                </a>
+              </div>
+              <br></br>
+              <button className="btn btn-light" onClick={handleBack}>Back</button>
+            </>
+          )}
+          {subscriptionPaid && articleData && (
+            // Display article content if subscription is paid
+            <div>
+              <h2>{articleData.title} ({formatDate(articleData.added)})</h2>
               <p>{articleData.shortinfo}</p>
               <p>{articleData.longinfo}</p>
+              <br></br>
+              <button className="btn btn-light" onClick={handleBack}>Back</button>
+
             </div>
-          ) : (
-            <p>Loading...</p>
-          )
-        ) : (
-          // Display message if user is not allowed to view article
+          )}
+        </div>
+      ) : (
+        // Display message if user is not allowed to view article
+        <>
+          <h2>{articleData?.title} ({articleData?.added ? formatDate(articleData.added) : ''})</h2>
           <div className="not-allowed-message">
             <h2>Restricted</h2>
             <p>
               You are not allowed to view this article on your subscription level. If you want to view it, you need to
               upgrade your plan.{" "}
               <a className="btn" href="/user/subscriptions">
-                upgrade plan
+                Upgrade plan
               </a>
             </p>
           </div>
-        )
-      ) : (
-        // Display message if user is not logged in
-        <div className="not-allowed-message">
-          <h2>Authentication Required</h2>
-          <p>You need to login to view this article.
-          {" "}
-              <a className="btn" href="/login">
-                Login
-              </a>
-          </p>
-        </div>
+          <br></br>
+          <button className="btn btn-light" onClick={handleBack}>Back</button>
+        </>
       )}
     </div>
   );
